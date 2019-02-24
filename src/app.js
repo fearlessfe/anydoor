aee771bcfcaa62bf47c6e05e36f332c495462c4d
Your Name <you@example.com>
2019-02-24 12:00:49 +0800

    finished

--- src/app.js
+++ src/app.js
@@ -1,19 +1,29 @@
 const http = require('http');
+const path = require('path');
+const chalk = require('chalk');
+
 const conf = require('./config/defaultConfig');
-const chalk = require('chalk')
+const route = require('./helper/route');
+const openUrl = require('./helper/openUrl');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<html>');
-  res.write('<body>');
-  res.write('Hello world!');
-  res.write('</body>');
-  res.write('</html>');
-  res.end();
-})
+class Server {
+  constructor(config) {
+    this.conf = Object.assign({}, conf, config)
+  }
 
-server.listen(conf.port, conf.hostname, () => {
-  const addr = `http://${conf.hostname}:${conf.port}`;
-  console.info(`Server started at ${chalk.green(addr)}`)
-})
+  start() {
+    const server = http.createServer((req, res) => {
+      const filePath = path.join(conf.root, req.url);  
+      route(req, res ,filePath, this.conf);    
+    })
+    
+    server.listen(this.conf.port, this.conf.hostname, () => {
+      const addr = `http://${this.conf.hostname}:${this.conf.port}`;
+      console.info(`Server started at ${chalk.green(addr)}`)
+      openUrl(addr);
+    })
+  }
+}
+
+module.exports = Server;
+
